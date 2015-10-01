21802df5e9e7911babb3aa7a72dd1689afefd222
Ben Strahan <ben@benstrahan.com>
2015-09-30 23:24:14 -0500

    Add ROOT_URL to redirect

--- server/redirects.js
+++ server/redirects.js
@@ -2,7 +2,7 @@
 _.each(Episodes.find().fetch(), function (episode) {
   var slug = "/e/" + episode.slug;
   WebApp.connectHandlers.use(slug, function (req, res, next) {
-    res.writeHead(301, {'Location': "https://crater.io/podcast/" + episode.slug});
+    res.writeHead(301, {'Location': process.env.ROOT_URL + "/podcast/" + episode.slug});
     res.end();
   });
 });
